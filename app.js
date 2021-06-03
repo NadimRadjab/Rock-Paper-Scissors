069a62135439d09b42ef5268f0ccb94cf4024080
Nadim Radjab <nadimradjab94@gmail.com>
2021-06-03 13:50:59 +0200

    Final

--- app.js
+++ app.js
@@ -1,81 +1,73 @@
 
-///function that the computer chose random items from my stuff array.
-
+// function for the computer selection
 function computerPlay() {
-    const random = stuff[Math.floor(Math.random() * stuff.length)]
-    cDiv.append(random)
+    const random = stuff[Math.floor(Math.random() * stuff.length)];
+    cDiv.append(random);
     return random
 };
 
 
 
-// const computer = {
-//     items: ['Rock', 'Paper', 'Scissors']
-// }
 
-
-
-
-
-
+/// Player and Computer sconre
 let playerScore = 0;
 let computerScore = 0;
 
 
-
-const h1 = document.querySelector('h1');
-const h2 = document.querySelector('#pScore');
-const h3 = document.querySelector('#cScore');
+///Dom elements
+const h2 = document.querySelector('h2');
+const pText = document.querySelector('#pScore');
+const cText = document.querySelector('#cScore');
 const gameDiv = document.querySelector('#game');
 const pDiv = document.querySelector('#pPic');
 const cDiv = document.querySelector('#cPic');
+const reset = document.querySelector('button');
+const bRock = document.querySelector('#rock');
+const bScissors = document.querySelector('#scissors')
+const bPaper = document.querySelector('#paper');
 
 
 
-const theGame = document.querySelector('div');
-const winner = document.querySelector('p');
-winner.classList.add('display');
+//Reset button
+reset.addEventListener('click', resetB);
 
-
-
-
-///My reset button that resets the score and togles classes
-const reset = document.querySelector('input');
-reset.classList.add('display');
-reset.addEventListener('click', () => {
+//Restart function
+function resetB() {
     playerScore = 0;
     computerScore = 0;
-    reset.classList.toggle('display');
-    winner.classList.toggle('display');
-    theGame.classList.toggle('opacity');
-    h1.innerText = 'Play a Game';
-    h2.innerText = 'Player Score: 0';
-    h3.innerText = 'Computer Score: 0';
-});
+    pDiv.innerHTML = '';
+    cDiv.innerHTML = '';
+    h2.innerText = 'Play a Game!';
+    pText.innerText = 'Player Score: 0';
+    cText.innerText = 'Computer Score: 0';
+    bRock.style.pointerEvents = 'auto';
+    bScissors.style.pointerEvents = 'auto';
+    bPaper.style.pointerEvents = 'auto';
+
+};
 
 
 
+//Element creation.
+const rockPic = document.createElement('img');
+rockPic.src = "pic/rock.jpg";
+rockPic.setAttribute("data-rock", 'Rock');
+const paperPic = document.createElement('img');
+paperPic.setAttribute("data-paper", "Paper");
+paperPic.src = "pic/paper.png";
+const scissorsPic = document.createElement('img');
+scissorsPic.setAttribute("data-scissors", "Scissors");
+scissorsPic.src = "pic/scissors.jpg";
 
 
-
-const rockPic = document.createElement('img')
-rockPic.src = "pic/rock.jpg"
-rockPic.setAttribute("data-rock", 'Rock')
-const paperPic = document.createElement('img')
-paperPic.setAttribute("data-paper", "Paper")
-paperPic.src = "pic/paper.png"
-const scissorsPic = document.createElement('img')
-scissorsPic.setAttribute("data-scissors", "Scissors")
-scissorsPic.src = "pic/scissors.jpg"
-
+///Array with the images.
 const stuff = [rockPic, paperPic, scissorsPic];
 
 
 
-
-const bRock = document.querySelector('#rock')
+///Button events.
 bRock.addEventListener('click', function () {
-    rockPic.remove()
+    rockPic.remove();
     paperPic.remove();
     scissorsPic.remove();
     pDiv.innerHTML = ''
@@ -84,36 +76,36 @@ bRock.addEventListener('click', function () {
 
     const playerSelection = rockPic;
     const computerSelection = computerPlay();
-    h1.innerText = game(playRound(playerSelection, computerSelection));
+    h2.innerText = game(playRound(playerSelection, computerSelection));
 
 });
 
-const bScissors = document.querySelector('#scissors')
+
 bScissors.addEventListener('click', () => {
-    scissorsPic.remove()
-    rockPic.remove()
-    paperPic.remove()
-    pDiv.innerHTML = ''
+    scissorsPic.remove();
+    rockPic.remove();
+    paperPic.remove();
+    pDiv.innerHTML = '';
 
     pDiv.innerHTML = `<img src="pic/scissors.jpg">`;
     const playerSelection = scissorsPic;
     const computerSelection = computerPlay();
 
-    h1.innerText = game(playRound(playerSelection, computerSelection));
+    h2.innerText = game(playRound(playerSelection, computerSelection));
 
 });
-const bPaper = document.querySelector('#paper')
+
 bPaper.addEventListener('click', () => {
-    paperPic.remove()
-    rockPic.remove()
-    scissorsPic.remove()
+    paperPic.remove();
+    rockPic.remove();
+    scissorsPic.remove();
     pDiv.innerHTML = ''
 
     pDiv.innerHTML = `<img src="pic/paper.png">`;
     const playerSelection = paperPic;
     const computerSelection = computerPlay();
 
-    h1.innerText = game(playRound(playerSelection, computerSelection));
+    h2.innerText = game(playRound(playerSelection, computerSelection));
 
 });
 
@@ -121,62 +113,60 @@ bPaper.addEventListener('click', () => {
 
 
 
+//function that plays the game.
 function playRound(playerSelection, computerSelection) {
-
 
     if (playerSelection === rockPic && computerSelection === stuff[1]) {
         computerScore++;
 
-        h3.innerText = `Computer Score: ${computerScore}`
+        cText.innerText = `Computer Score: ${computerScore}`
         return 'Paper beats Rock: You lose Computer wins!'
     } else if (playerSelection === paperPic && computerSelection === stuff[0]) {
         playerScore++;
 
-        h2.innerText = `Player Score: ${playerScore}`;
+        pText.innerText = `Player Score: ${playerScore}`;
         return 'Paper beats Rock: You win Computer loses!';
     } else if (playerSelection === paperPic && computerSelection === stuff[2]) {
         computerScore++;
 
-        h3.innerText = `Computer Score: ${computerScore}`;
+        cText.innerText = `Computer Score: ${computerScore}`;
         return 'Scissors beats Paper: You lose Computer wins!';
     } else if (playerSelection === scissorsPic && computerSelection === stuff[1]) {
         playerScore++;
 
-        h2.innerText = `Player Score: ${playerScore}`;
+        pText.innerText = `Player Score: ${playerScore}`;
         return ' Scissors beats Paper: You win Computer loses!';
 
     } else if (playerSelection === rockPic && computerSelection === stuff[2]) {
         playerScore++;
 
-        h2.innerText = `Player Score: ${playerScore}`;
+        pText.innerText = `Player Score: ${playerScore}`;
         return 'Rock beats Scissors: You win Computer loses!';
 
     } else if (playerSelection === scissorsPic && computerSelection === stuff[0]) {
         computerScore++;
 
-        h3.innerText = `Computer Score: ${computerScore}`;
+        cText.innerText = `Computer Score: ${computerScore}`;
         return 'Rock beats Scissors: You lose Computer wins!';
 
     } else return "Even Play Again!";
 
 
-
 };
 
 
-
+//function that decides the winner.
 function game(play) {
-    if (playerScore >= 100 || computerScore >= 100) {
-        winner.classList.toggle('display')
-        theGame.classList.toggle('opacity')
-        reset.classList.toggle('display')
+    if (playerScore >= 5 || computerScore >= 5) {
+        bRock.style.pointerEvents = 'none';
+        bScissors.style.pointerEvents = 'none';
+        bPaper.style.pointerEvents = 'none';
     }
-    if (playerScore >= 100) {
+    if (playerScore >= 5) {
+        return h2.innerText = 'You win Computer Lost!';
 
-        winner.textContent = 'You win Computer has Lost!'
-    } else if (computerScore >= 100) {
-
-        winner.textContent = 'You lost Computer has Won!'
+    } else if (computerScore >= 5) {
+        return h2.innerText = 'You lost Computer Won!';
     }
     return play
 };
