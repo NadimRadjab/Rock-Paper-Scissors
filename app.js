faf74353fe33c6eeb35d452486a4afd85652744d
Nadim Radjab <nadimradjab94@gmail.com>
2021-06-03 12:25:11 +0200

    Finishing

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-/// Array with my stuff inside .
-const stuff = ['Rock', 'Paper', 'Scissors'];
+
 ///function that the computer chose random items from my stuff array.
+
 function computerPlay() {
     const random = stuff[Math.floor(Math.random() * stuff.length)]
+    cDiv.append(random)
     return random
 };
-/// Player score and computer score
+
+
+
+// const computer = {
+//     items: ['Rock', 'Paper', 'Scissors']
+// }
+
+
+
+
+
+
 let playerScore = 0;
 let computerScore = 0;
 
 
 
-///Selecting my Hs
 const h1 = document.querySelector('h1');
 const h2 = document.querySelector('#pScore');
 const h3 = document.querySelector('#cScore');
-const gameDiv = document.querySelector('#game')
-const pDiv = document.querySelector('#pPic')
-const cDiv = document.querySelector('#cPic')
+const gameDiv = document.querySelector('#game');
+const pDiv = document.querySelector('#pPic');
+const cDiv = document.querySelector('#cPic');
 
 
 
-/// Selecting div and p so I can add display and toggle class
 const theGame = document.querySelector('div');
 const winner = document.querySelector('p');
 winner.classList.add('display');
@@ -50,56 +60,57 @@ reset.addEventListener('click', () => {
 
 const rockPic = document.createElement('img')
 rockPic.src = "pic/rock.jpg"
+rockPic.setAttribute("data-rock", 'Rock')
 const paperPic = document.createElement('img')
+paperPic.setAttribute("data-paper", "Paper")
 paperPic.src = "pic/paper.png"
 const scissorsPic = document.createElement('img')
+scissorsPic.setAttribute("data-scissors", "Scissors")
 scissorsPic.src = "pic/scissors.jpg"
-// const pDiv = document.createElement('div')
-// const cDiv = document.createElement('div')
-// pDiv.classList.add('test')
-// cDiv.classList.add('test')
 
-
-// gameDiv.append(pDiv)
-// gameDiv.append(cDiv)
+const stuff = [rockPic, paperPic, scissorsPic];
 
 
 
 
-
-
-const bRock = document.querySelector('#r')
+const bRock = document.querySelector('#rock')
 bRock.addEventListener('click', function () {
     rockPic.remove()
-    paperPic.remove()
-    scissorsPic.remove()
-    pDiv.append(rockPic)
-    const playerSelection = 'rock';
-    const computerSelection = computerPlay();
+    paperPic.remove();
+    scissorsPic.remove();
+    pDiv.innerHTML = ''
 
+    pDiv.innerHTML = `<img src="pic/rock.jpg">`;
+
+    const playerSelection = rockPic;
+    const computerSelection = computerPlay();
     h1.innerText = game(playRound(playerSelection, computerSelection));
 
 });
 
-const bScissors = document.querySelector('#s')
+const bScissors = document.querySelector('#scissors')
 bScissors.addEventListener('click', () => {
+    scissorsPic.remove()
     rockPic.remove()
     paperPic.remove()
-    scissorsPic.remove()
-    pDiv.append(scissorsPic)
-    const playerSelection = 'scissors';
+    pDiv.innerHTML = ''
+
+    pDiv.innerHTML = `<img src="pic/scissors.jpg">`;
+    const playerSelection = scissorsPic;
     const computerSelection = computerPlay();
 
     h1.innerText = game(playRound(playerSelection, computerSelection));
 
 });
-const bPaper = document.querySelector('#p')
+const bPaper = document.querySelector('#paper')
 bPaper.addEventListener('click', () => {
-    rockPic.remove()
     paperPic.remove()
+    rockPic.remove()
     scissorsPic.remove()
-    pDiv.append(paperPic)
-    const playerSelection = 'paper';
+    pDiv.innerHTML = ''
+
+    pDiv.innerHTML = `<img src="pic/paper.png">`;
+    const playerSelection = paperPic;
     const computerSelection = computerPlay();
 
     h1.innerText = game(playRound(playerSelection, computerSelection));
@@ -110,71 +121,60 @@ bPaper.addEventListener('click', () => {
 
 
 
-
-/////////////My shitty function with alot of code that needs to be fixed I think.It takes a player selection and computer selection and returns the outcome of my if statements
 function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection === 'rock' && computerSelection === 'Paper') {
-        computerScore++
 
-        cDiv.append(paperPic)
+    if (playerSelection === rockPic && computerSelection === stuff[1]) {
+        computerScore++;
 
         h3.innerText = `Computer Score: ${computerScore}`
         return 'Paper beats Rock: You lose Computer wins!'
-    } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
-        playerScore++
+    } else if (playerSelection === paperPic && computerSelection === stuff[0]) {
+        playerScore++;
 
-        cDiv.append(rockPic)
+        h2.innerText = `Player Score: ${playerScore}`;
+        return 'Paper beats Rock: You win Computer loses!';
+    } else if (playerSelection === paperPic && computerSelection === stuff[2]) {
+        computerScore++;
 
-        h2.innerText = `Player Score: ${playerScore}`
-        return 'Paper beats Rock: You win Computer loses!'
-    } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
-        computerScore++
+        h3.innerText = `Computer Score: ${computerScore}`;
+        return 'Scissors beats Paper: You lose Computer wins!';
+    } else if (playerSelection === scissorsPic && computerSelection === stuff[1]) {
+        playerScore++;
 
-        cDiv.append(scissorsPic)
+        h2.innerText = `Player Score: ${playerScore}`;
+        return ' Scissors beats Paper: You win Computer loses!';
 
-        h3.innerText = `Computer Score: ${computerScore}`
-        return 'Scissors beats Paper: You lose Computer wins!'
-    } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
-        playerScore++
+    } else if (playerSelection === rockPic && computerSelection === stuff[2]) {
+        playerScore++;
 
-        cDiv.append(paperPic)
-        h2.innerText = `Player Score: ${playerScore}`
-        return ' Scissors beats Paper: You win Computer loses!'
+        h2.innerText = `Player Score: ${playerScore}`;
+        return 'Rock beats Scissors: You win Computer loses!';
 
-    } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
-        playerScore++
+    } else if (playerSelection === scissorsPic && computerSelection === stuff[0]) {
+        computerScore++;
 
-        cDiv.append(scissorsPic)
-        h2.innerText = `Player Score: ${playerScore}`
-        return 'Rock beats Scissors: You win Computer loses!'
+        h3.innerText = `Computer Score: ${computerScore}`;
+        return 'Rock beats Scissors: You lose Computer wins!';
 
-    } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
-        computerScore++
+    } else return "Even Play Again!";
 
-        cDiv.append(rockPic)
-        h3.innerText = `Computer Score: ${computerScore}`
-        return 'Rock beats Scissors: You lose Computer wins!'
-
-    } else if (playerSelection == computerSelection) return "Even Play Again!"
 
 
 };
 
 
 
-////my game function that works to decide a winner the winner is deice by who reaches a score of 3 first.I don't really stop the game I just toggle classes so I can blur the backgroung
-//// and show the winner and to show a reset button the button resets the score ,but if the player presses for example other button can crash the game or simply put it not really a polisd product
 function game(play) {
-    if (playerScore >= 3 || computerScore >= 3) {
+    if (playerScore >= 100 || computerScore >= 100) {
         winner.classList.toggle('display')
         theGame.classList.toggle('opacity')
         reset.classList.toggle('display')
     }
-    if (playerScore >= 3) {
+    if (playerScore >= 100) {
 
         winner.textContent = 'You win Computer has Lost!'
-    } else if (computerScore >= 3) {
+    } else if (computerScore >= 100) {
 
         winner.textContent = 'You lost Computer has Won!'
     }
